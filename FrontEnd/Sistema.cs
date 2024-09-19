fad160f7fccea307077a545810e36f211277890f
Victorhugorodriguescost <163910434+victorhrc242@users.noreply.github.com>
2024-09-19 13:57:41 -0300

    19/09/24 13:51

--- FrontEnd/Sistema.cs
+++ FrontEnd/Sistema.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using FrontEnd.UseCases;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -7,9 +8,19 @@ using System.Threading.Tasks;
 namespace FrontEnd;
 public class Sistema
 {
+    private readonly UsuarioUC _usuarioUC;
+    public Sistema()
+    {
+           _usuarioUC= new UsuarioUC();
+    }
     public void IniciarSistema()
     {
         int resposta = ExibirLogin();
+        if(resposta == 0)
+        {
+            Usuario usuario =new Usuario();
+            _usuarioUC.CadastrarUsuario(usuario);
+        }
     }
     public int ExibirLogin()
     {
@@ -18,4 +29,17 @@ public class Sistema
         Console.WriteLine("2 - Deseja se Cadastrar");
         return int.Parse(Console.ReadLine());
     }
+    public Usuario criarusuario()
+    {
+        Usuario usuario= new Usuario();
+        Console.WriteLine("Digite seu nome");
+        usuario.nome = Console.ReadLine();
+        Console.WriteLine("Digite seu username");
+        usuario.username = Console.ReadLine();
+        Console.WriteLine("Digite sua senha");
+        usuario.senha = Console.ReadLine();
+        Console.WriteLine("Digite seu email");
+        usuario.email = Console.ReadLine();
+        return usuario;
+    } 
 }
